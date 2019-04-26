21e0430dc8e9369c8bc3c7c3e0242529e55e6122
DevelopMan <47355278+ControlWebManager@users.noreply.github.com>
2019-04-26 14:55:47 -0400

    init

---
+++ website_sale_social_share_siki/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Social Share Ecommerce SIKI',
+    'summary': 'Share contents in social networks',
+    'depends': [
+        'website',
+        'website_sale',
+    ],
+    'author': 'SIKI SAS, Developer Ing Henry Vivas',
+    'website': 'www.sikisoftware.com',
+    "support": "controlwebmanager@gmail.com",
+    'category': 'Website',
+    'description': """
+Share products on social networks 
+=================================
+
+Social Media
+    * Facebook, 
+    * Twitter, 
+    * Pinterest 
+    * Whatsapp
+       
+List of modifications:
+----------------------
+    * V.-1.0 Modify and improve functionality buttons compatir of products in stores( Req. 1063 )
+            
+    """,
+    'data': [
+        'views/website.xml',
+        'views/website_sale.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'qweb': [
+    ],
+    'js': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+}
